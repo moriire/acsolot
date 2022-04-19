98db89d28465d2a421e6359a6bb8b3b8d6bd021b
Mobolaji Abdulsalam <ibraheemabdulsalam@gmail.com>
2022-04-19 18:45:55 +0100

    Update content.js

--- components/content.js
+++ components/content.js
@@ -1,3 +1,13 @@
-const Link = ReactRouterDOM.Link                      const Route = ReactRouterDOM.Route;                                                                         const App = () => (                                   <ReactRouterDOM.HashRouter>                           <Route path="/" exact component={Home} />             <Route path="/about" component={About} />             <Route path="/contact" component={Contact} />
-</ReactRouterDOM.HashRouter>                                                                                );                                                                                                          const Home = () =><h1>Homw</h1>                       const About = () =><h1>About</h1>                     const Contact = () =><h1>Register</h1>;               
+const Link = ReactRouterDOM.Link
+const Route = ReactRouterDOM.Route;                                                                         
+const App = () => (                                   
+<ReactRouterDOM.HashRouter>                           
+<Route path="/" exact component={Home} />             
+<Route path="/about" component={About} />             
+<Route path="/contact" component={Contact} />
+</ReactRouterDOM.HashRouter>                                                                                
+);                                                                                                          
+const Home = () =><h1>Homw</h1>                       
+const About = () =><h1>About</h1>                     
+const Contact = () =><h1>Register</h1>;               
 ReactDOM.render(<App />, document.querySelector('#root'));
