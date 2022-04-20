9acbe7b62d7c570dbbe9b7844b82e19150a4f04e
moriire <ibraheemabdulsalam@gmail.com>
2022-04-20 16:21:35 +0100

    update services-web

--- components/content.js
+++ components/content.js
@@ -1,13 +1,13 @@
 const Link = ReactRouterDOM.Link
-const Route = ReactRouterDOM.Route;                                                                         
-const App = () => (                                   
+const Route = ReactRouterDOM.Route;                                             
+const App = () => (                           
 <ReactRouterDOM.HashRouter>                           
 <Route path="/" exact component={Home} />             
 <Route path="/about" component={About} />             
 <Route path="/contact" component={Contact} />
 <Route path="/services" component={Services} />
 <Route path="/web" component={Web} />
-</ReactRouterDOM.HashRouter>                                                                                
+</ReactRouterDOM.HashRouter>        
 );
 
 ReactDOM.render(<App />, document.querySelector('#root'));
@@ -16,8 +16,7 @@ const Home = () =><h1>Homw</h1>
 
 const Services = () =><h1>Homw</h1>
 
-const Contact = () =><h1>Register</h1>;               
-ReactDOM.render(<App />, document.querySelector('#root'));
+const Contact = () =><h1>Register</h1>;  
 
 function About(){
 	return (
