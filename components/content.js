ef4b4ba8290b06fc0568d56f9da1f048ec1a9da2
moriire <ibraheemabdulsalam@gmail.com>
2022-04-20 13:19:51 +0100

    update index

--- components/content.js
+++ components/content.js
@@ -10,7 +10,7 @@ const App = () => (
 const Home = () =><h1>Homw</h1>                       
 const About = () =>(
 	<section class="my-3 py-3">                       <div class="container">                           <div class="row">                             <div class="row justify-content-center text-center my-sm-5">                              <div class="col-lg-6">                        <span class="badge bg-primary mb-3">About</span>                        <h2 class="text-dark mb-0">Who We Are</h2>                             <p class="lead">         <p>Acsolot Integrated Services Limited formally Ages Xpat Technology is a NG-based IT-consulting and software development company founded in the year 2009; out of burning passion to help global companies and established brands to achieve and accelerates business growth at a reduced costs.</p>       </p>                                                                                                  </div>                                    </div>                                    </div>                                    </div>                </section>
+	
 )
-
 const Contact = () =><h1>Register</h1>;               
 ReactDOM.render(<App />, document.querySelector('#root'));
